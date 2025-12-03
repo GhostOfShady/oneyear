aaabe9d152a9e2297b99bab330746cf786acefd1
Shadoso <djscraft360@gmail.com>
2025-12-03 17:36:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const spawner = {
+    x: [-5, 105],//vw
+    y: 105//vh
+}
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(() => {
+        const div = document.createElement('div')
+
+        div.style.left =    (~~(Math.random() * (spawner.x[1] - spawner.x[0] + 1)) + spawner.x[0]) + 'vw';
+        div.style.top = spawner.y + 'vh'
+        div.className = 'heart'
+
+        div.addEventListener('animationend', () => div.remove())
+
+        document.body.appendChild(div)
+    }, 250)
+})
