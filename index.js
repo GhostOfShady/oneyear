bbaad16c7062d6422ce92ae78bf536642e4d6147
Shadoso <djscraft360@gmail.com>
2025-12-04 14:56:33 -0300

    capybara and daisy

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 document.documentElement.style.setProperty('--h', window.innerHeight + 'px');
 
+const [width, height] = [window.innerWidth, window.innerHeight]
 const spawner = {
-    x: [-5, 105],//vw
-    y: 105//vh
+    x: [width *  -0.05, width * 1.05],//vw
+    y: height * 1.05//vh
 }
 document.addEventListener('DOMContentLoaded', () => {
     setInterval(() => {
         if (!document.hasFocus()) return
         const div = document.createElement('div')
 
-        div.style.left =    (~~(Math.random() * (spawner.x[1] - spawner.x[0] + 1)) + spawner.x[0]) + 'vw';
-        div.style.top = spawner.y + 'vh'
+        div.style.left =    (~~(Math.random() * (spawner.x[1] - spawner.x[0] + 1)) + spawner.x[0]) + 'px';
+        div.style.top = spawner.y + 'px'
         div.className = 'heart'
 
         div.addEventListener('animationend', () => div.remove())
